9c2df6cb47245f33230e0d363c3806964d239ae3
ShaQay3Q <shaghay3gh@gmail.com>
2024-09-19 15:14:02 +0200

    callback func

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,28 +4,33 @@ let finalResult = 0;
 const defaultValue = 0;
 let currentResult = defaultValue;
 
-function add (){
-    // const result = n1 + n2;
-    // alert(`the result is ${result}`)
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, "");
+function add(n1, n2) {
+	// const result = n1 + n2;
+	// alert(`the result is ${result}`)
+	currentResult = currentResult + userInput.value;
+	// currentResultOutput.textContent = currentResult;
+	outputResult(currentResult, "");
 }
 
-addBtn.addEventListener("click", add)
+addBtn.addEventListener("click", add);
 
+subtractBtn.addEventListener("click", () => {
+	currentResult = currentResult - userInput.value;
+	outputResult(currentResult, "");
+});
 
-function sub (n1, n2){
-    return n1 - n2;
+function sub(n1, n2) {
+	return n1 - n2;
 }
 
-function multi (n1, n2){
-    return n1 * n2;
+function multi(n1, n2) {
+	return n1 * n2;
 }
 
-function div (n1, n2){
-    if (!n2){
-        return n1 / n2;
-    } else {
-        return "inf"
-    }
+function div(n1, n2) {
+	if (!n2) {
+		return n1 / n2;
+	} else {
+		return "inf";
+	}
 }
