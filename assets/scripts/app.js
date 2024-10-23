da215c83f9194e8c34d4372087bce7624a8fb8ad
ShaQay3Q <shaghay3gh@gmail.com>
2024-10-23 23:01:40 +0200

    reset and focus

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,5 @@
 // alert("this shit works!");
 
-const { userInfo, type } = require("os");
-
 // variables defined at the global scope
 let finalResult = 0;
 const defaultValue = 0;
@@ -10,7 +8,8 @@ let currentResult = defaultValue;
 function add() {
 	currentResult = currentResult + Number(userInput.value);
 	outputResult(currentResult, "");
-	// userInfo.value = "";
+	userInput.value = ""; // Clear the input field after addition
+	userInput.focus();
 }
 
 addBtn.addEventListener("click", add);
@@ -18,20 +17,31 @@ addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", () => {
 	currentResult = currentResult - Number(userInput.value);
 	outputResult(currentResult, "");
-	// userInfo.value = "";
+	userInput.value = ""; // Clear the input field after addition
+	userInput.focus();
 });
 
-function div() {
-	type = typeof userInput.value;
-	n = Number(userInput.value);
-	if (!typeof type === "number" && !n) {
-		if (!n) {
-			currentResult = currentResult / n;
-		} else {
-			currentResult = "inf";
-		}
-	} else {
-		currentResult;
-	}
-	return currentResult;
+function mult() {
+	currentResult = currentResult * Number(userInput.value);
+	outputResult(currentResult, "");
+	userInput.value = ""; // Clear the input field after addition
+	userInput.focus();
 }
+
+function divi() {
+	console.log(`userInputValue = ${userInput.value}`);
+
+	let n = Number(userInput.value);
+
+	if (n !== 0) {
+		currentResult = currentResult / n;
+	} else {
+		currentResult = "inf";
+	}
+	outputResult(currentResult, "");
+	userInput.value = ""; // Clear the input field after addition
+	userInput.focus();
+}
+
+divideBtn.addEventListener("click", divi);
+multiplyBtn.addEventListener("click", mult);
