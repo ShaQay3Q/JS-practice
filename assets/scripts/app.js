72f819a28612fd04fa4ecb5a304f49f5140e4d27
ShaQay3Q <shaghay3gh@gmail.com>
2024-10-28 17:09:53 +0100

    still works

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,13 +5,18 @@ let currentResult = defaultValue;
 userInput.value = "";
 userInput.focus();
 
+function getUsrInptNo() {
+	return Number(userInput.value);
+}
+
+const getUsrNoInpt = () => Number(userInput.value);
+
 function add() {
-	const entrdNo = Number(userInput.value);
-	const calcDescription = `${currentResult} + ${entrdNo}`;
-	currentResult = currentResult + entrdNo;
+	const calcDescription = `${currentResult} + ${getUsrNoInpt()}`;
+	currentResult = currentResult + getUsrNoInpt();
 	console.log(
 		`type of Number(userInput.value) is: ${typeof Number(userInput.value)}
-		and it's value is: ${entrdNo}`
+		and it's value is: ${getUsrNoInpt()}`
 	);
 
 	outputResult(currentResult, calcDescription);
@@ -22,34 +27,30 @@ function add() {
 addBtn.addEventListener("click", add);
 
 subtractBtn.addEventListener("click", () => {
-	const entrdNo = Number(userInput.value);
-	const calcDescription = `${currentResult} - ${entrdNo}`;
+	// const entrdNo = Number(userInput.value);
+	const calcDescription = `${currentResult} - ${getUsrNoInpt()}`;
 
-	currentResult = currentResult - entrdNo;
+	currentResult = currentResult - getUsrNoInpt();
 	outputResult(currentResult, calcDescription);
 	userInput.value = "";
 	userInput.focus();
 });
 
 function mult() {
-	const entrdNo = Number(userInput.value);
-	const calcDescription = `${currentResult} * ${entrdNo}`;
+	const calcDescription = `${currentResult} * ${getUsrNoInpt()}`;
 
-	currentResult = currentResult * entrdNo;
+	currentResult = currentResult * getUsrNoInpt();
 	outputResult(currentResult, calcDescription);
 	userInput.value = "";
 	userInput.focus();
 }
 
 function divi() {
-	const entrdNo = Number(userInput.value);
-	const calcDescription = `${currentResult} / ${userInput.value}`;
-	console.log(`userInputValue = ${userInput.value}`);
+	const calcDescription = `${currentResult} / ${getUsrNoInpt()}`;
+	console.log(`userInputValue = ${getUsrNoInpt()}`);
 
-	// let n = Number(userInput.value);
-
-	if (entrdNo !== 0) {
-		currentResult = currentResult / entrdNo;
+	if (getUsrNoInpt() !== 0) {
+		currentResult = currentResult / getUsrNoInpt();
 	} else {
 		currentResult = "inf";
 	}
